4bb8708ad4c84245879a5a8a72b8a17d0352c860
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-02-04 21:41:27 -0600

    Agregar estructura de la clase interfaz

---
+++ interfaz.py
@@ -0,0 +1,21 @@
+# import Clientes from clientes
+# import Cuentas_Bancarias from cuentas_bacarias
+# import Prestamo from prestamos
+# import Informacion_Clientes from informacion_clientes
+
+class Interfaz:
+    
+    def crear_cuenta():
+        # Codigo 
+    
+    def solicitar_cuenta():
+        # Codigo 
+        
+    def solicitar_prestamo():
+        # Codigo
+        
+    def informacion():
+        # Codigo
+        
+        
+        
