317600ca9b7481e285a075db4ec799bba8f7e3c1
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-02-05 18:41:11 -0600

    Corregir lenguaje. Cambio de .py a c++

---
+++ interfaz.cpp
@@ -0,0 +1,34 @@
+// #include "Clientes.hpp"
+// #include "CuentasBancarias.hpp"
+// #include "Prestamos.hpp"
+// #include "informacionClientes.hpp"
+
+class Interfaz {
+private:
+    // Clientes clientes;
+    // CuentasBancarias cuentas_bancarias;
+    // Prestamos prestamos;
+    // InformacionClientes informacion_clientes;
+
+public:
+    Interfaz() {
+        
+    }
+
+    void crear_cuenta(int tipo_cuenta, int cliente_id) {
+       
+    }
+
+    void solicitar_cuenta() {
+        
+    }
+
+    void solicitar_prestamo() {
+        
+    }
+
+    void informacion() {
+        
+    }
+};
+
