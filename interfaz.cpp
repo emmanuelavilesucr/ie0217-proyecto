005e6835b852f80b934deccdac1d73e4ff3244c4
BraiderZ <luisjbc2112@gmail.com>
2024-02-07 08:51:08 -0600

    Primeros pasos para el menú de atención al cliente
    Signed-off-by: BraiderZ <luisjbc2112@gmail.com>

--- interfaz.cpp
+++ interfaz.cpp
@@ -1,5 +1,31 @@
 #include "interfaz.hpp" 
 
+void Interfaz::menu_atencion_clientes(){
+    int opcion_cliente;
+    
+    std::cout << "---Opciones para la atención al cliente---" << std::endl;
+    std::cout << "1. Acceder a una cuenta creada" << std::endl;
+    std::cout << "2. Crear una cuenta" << std::endl;
+    std::cout << "Ingrese el número de una opción: ";
+
+    std::cin >> opcion_cliente;
+        
+        switch (opcion_cliente)
+        {
+        case 1:
+            std::cout << "Se ha elegido la opcion 1" << std::endl;
+            //Invocar funcionalidad para crear cuenta
+            break;
+        case 2:
+            std::cout << "Opcion 2";
+            //Incoar la opcion de elegir un usuario y las acciones que puede realizar
+            break;
+        default:
+            throw std::runtime_error("La opción elegida no esta disponible.");
+        }
+
+}
+
 void Interfaz::menu_inicial(){
     int opcion;
 
@@ -17,7 +43,7 @@ void Interfaz::menu_inicial(){
             std::cout << "Se ha elegido la opcion 1" << std::endl;
             break;
         case 2:
-            std::cout << "Se ha elegido la opcion 2" << std::endl;
+            menu_atencion_clientes();
             break;
         default:
             throw std::runtime_error("La opción elegida no esta disponible.");
