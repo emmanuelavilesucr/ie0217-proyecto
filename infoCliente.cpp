7d96e0e088b912f8e6c6f6b32f967f872e80b6e9
brendacj <brenda.castrojimenez@ucr.ac.cr>
2024-02-08 05:02:01 -0600

    Agrego la implementación del método calcular amortización de la clase infoCliente
    Signed-off-by: brendacj <brenda.castrojimenez@ucr.ac.cr>

--- infoCliente.cpp
+++ infoCliente.cpp
@@ -4,17 +4,44 @@ InfoCliente::InfoCliente(float montoPrestamo, float tasaInteresAnual, int numCuo
     this->montoPrestamo = montoPrestamo;
     this->tasaInteresAnual = tasaInteresAnual;
     this->numCuotas = numCuotas;
+    cout << "xd";
+    amortizacion = calcularAmortizacion();
 }
 
-std::vector<float> InfoCliente::calcularAmortizacion(){
+std::vector<std::vector<float>> InfoCliente::calcularAmortizacion(){
+    try{
+        if (numCuotas == 0){
+            throw runtime_error("Error: El número de cuotas no puede ser cero");
+        }
+        float tasaInteresMensual = tasaInteresAnual / 12 / 100;
+        float cuotaMensual = (tasaInteresMensual * montoPrestamo) / (1 - pow((1 + tasaInteresMensual), -numCuotas));
+        float saldoRestante = montoPrestamo;
+        std::vector<std::vector<float>> amortizacion;
+
+        for (float cuota = 1; cuota < numCuotas + 1; cuota ++){
+            float interesPendiente = saldoRestante * tasaInteresMensual;
+            float amortizacionPrincipal = cuotaMensual - interesPendiente;
+            saldoRestante -= amortizacionPrincipal;
+            std::vector<float> vectorTemp = {cuota, interesPendiente, amortizacionPrincipal, saldoRestante};
+            amortizacion.push_back(vectorTemp);
+        }
+
+        return amortizacion;
+    }
+    catch(const exception& e){
+        cerr << e.what() << endl;
+        std::vector<float> vectorTemp = {};
+        amortizacion.push_back(vectorTemp);
+        return amortizacion;
+    }
+}
+
+void InfoCliente::generarReporte(std::string nombre){
     
 }
 
-void InfoCliente::generarReporte(){
-
-}
-
 void InfoCliente::menuPrincipal(){
+
 
 }
 
--- infoCliente.hpp
+++ infoCliente.hpp
@@ -1,6 +1,9 @@
 #ifndef INFORMACION_HPP
 #define INFORMACION_HPP
 #include <vector>
+#include <stdexcept>
+#include <cmath>
+#include <fstream>
 
 #include <iostream>
 using namespace std;
@@ -8,8 +11,8 @@ using namespace std;
 class InfoCliente {
     public:
         InfoCliente(float montoPrestamo, float tasaInteresAnual, int cuotas);
-        std::vector<float> calcularAmortizacion();
-        void generarReporte();
+        std::vector<std::vector<float>> calcularAmortizacion();
+        void generarReporte(std::string nombre);
         void menuPrincipal();
         void procesarOpcion1();
         void menuSecundario();
@@ -22,6 +25,7 @@ class InfoCliente {
         float montoPrestamo;
         float tasaInteresAnual;
         int numCuotas;
+        std::vector<std::vector<float>> amortizacion;
 };
 
 #endif
