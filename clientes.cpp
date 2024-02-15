b4f0ee79229c802decc7f99d665445b3d4de5338
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-02-14 20:48:01 -0600

    Agregar documentacion de la clase clientes

--- clientes.cpp
+++ clientes.cpp
@@ -5,12 +5,18 @@
 
 Clientes::Clientes(long long int id, const std::string& nombre) : cliente_id(id), nombre(nombre) {}
 
+/**
+ * 
+ * Esta funcion guarda el ID y el nombre del cliente. 
+ * 
+ * @brief Si el archivo no existe, el programa creara el archivo
+ */
 void Clientes::GuardarInformacion() const {
 
     std::cout << "ID del Cliente: " << cliente_id << ", Nombre: " << nombre << std::endl;
     std::ofstream archivo("clientes.txt", std::ios::app);
     if (archivo.is_open()) {
-        archivo << cliente_id << "," << nombre << std::endl;
+        archivo << cliente_id << "," << nombre << std::endl;  // Escribe los datos del cliente en archivo txt
         archivo.close();
     } else {
         std::cerr << "No se pudo abrir el archivo para escribir." << std::endl;
