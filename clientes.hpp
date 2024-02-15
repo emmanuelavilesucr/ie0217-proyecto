7d8d556bec58f66b8ec616374ffc6e30192d16bf
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-02-14 20:47:49 -0600

    Agregar documentacion de la clase clientes

--- clientes.hpp
+++ clientes.hpp
@@ -2,14 +2,23 @@
 #define CLIENTES_HPP
 #include <string>
 
+
+/**
+ * Clase encargada del registro de un nuevo usuario. Esta clase extrae el ID y su nombre del cliente y lo almacena en un archivo txt.
+ * 
+ */
 class Clientes {
+
 private:
     long long int cliente_id;
     std::string nombre;
 
 public:
+
+
     Clientes(long long int id, const std::string& nombre); 
-    void GuardarInformacion() const;
+
+    void GuardarInformacion() const; // Esta funcion guarda los datos en un archivo llamado txt.  
 };
 
 #endif
