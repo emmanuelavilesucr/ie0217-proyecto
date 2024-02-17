b7ac5aa74d0e00a968770840a933e5ef2a9a2295
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-02-17 15:24:35 -0600

    Agregar estrutura de datos.cpp y datos.hpp

---
+++ datos.cpp
@@ -0,0 +1,10 @@
+#include "datos.hpp"
+#include <iostream>
+#include <fstream> 
+
+Datos::Datos() : {}
+
+void Datos::ingresarDatos() const {
+
+
+}
---
+++ datos.hpp
@@ -0,0 +1,15 @@
+#ifndef DATOS_HPP
+#define DATOS_HPP
+
+class Datos {
+
+private:
+
+
+public:
+    
+    void ingresarDatos() const;  
+
+};
+
+#endif
