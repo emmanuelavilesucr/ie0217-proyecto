6039d59c29c9d29b2f95f1d5b684c4a01b0c927d
emmanuelavilesucr <emmanuel.aviles@ucr.ac.cr>
2024-02-18 15:45:11 -0600

    Se agrego un main provisional para realizar pruebas del archivo txt

--- cdp.cpp
+++ cdp.cpp
@@ -3,8 +3,7 @@
 #include <fstream>
 
 
-
-void CDP::ingresarCDP() const {
+void CDP::ingresarCDP() {
     
  // std::cout << "ID de Cliente: " << cliente_id << ", Nombre: " << nombre << std::endl;
 
@@ -16,3 +15,36 @@ void CDP::ingresarCDP() const {
         std::cerr << "No se pudo abrir el archivo para escribir." << std::endl;
     }
 }
+
+
+// main provisional
+
+int main() {
+    
+    CDP miCDP;
+
+    long long int idCuenta;
+    std::cout << "Ingrese el ID de la cuenta: ";
+    std::cin >> idCuenta;
+    miCDP.setIdCuenta(idCuenta);
+
+
+    double interes;
+    std::cout << "Ingrese el interes: ";
+    std::cin >> interes;
+    miCDP.setInteres(interes);
+
+    double monto;
+    std::cout << "Ingrese el monto: ";
+    std::cin >> monto;
+    miCDP.setMontoCDP(monto);
+
+    miCDP.setTipo("Colones");
+    miCDP.ingresarCDP();
+
+    std::cout << "Se ha ingresado el CDP en el archivo." << std::endl;
+
+    return 0;
+}
+
+
--- cdp.hpp
+++ cdp.hpp
@@ -1,9 +1,10 @@
 #ifndef CDP_HPP
 #define CDP_HPP
+
 #include <string>
 
+// Se debe heredar de la clase Cuentas Bancarias
 class CDP {
-
 private:
     long long int idCuenta;
     double interes; 
@@ -11,12 +12,14 @@ private:
     std::string tipo;
 
 public:
+    
+    void setIdCuenta(long long int id) { idCuenta = id; }
+    void setInteres(double nuevoInteres) { interes = nuevoInteres; }
+    void setMontoCDP(double monto) { montoCDP = monto; }
+    void setTipo(const std::string& t) { tipo = t; }
 
-    void ingresarCDP() const;
+
+    void ingresarCDP();
 };
 
 #endif
-
-
-
-
