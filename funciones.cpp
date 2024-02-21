5b99ac4ff69e1f61ad4b142a15a745d8dccb66a8
BraiderZ <luisjbc2112@gmail.com>
2024-02-21 11:11:02 -0600

    El código fue documentado
    Signed-off-by: BraiderZ <luisjbc2112@gmail.com>

--- funciones.cpp
+++ funciones.cpp
@@ -1,5 +1,37 @@
 #include "funciones.hpp"
+/**
+ * @file funciones.cpp
+ * @brief Lógica de las funciones variadas. 
+ * 
+ *
+ * @license 
+ * Copyright 2024 [Brenda Natalia Castro Jiménez, Emanuel Avilés Ramírez, Luis José Brenes Campos]
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+*/
 
+/**
+ * @brief Convierte un tipo de moneda en otra
+ * 
+ * @param dinero Dinero que se desea convertir
+ * @param tipo_moneda Tipo de moneda del dinero antes de la conversión
+ * 
+ * La conversión se basó en el precio del dolar en el momento de la creación de este archivo
+ * 
+ * Retorna el dinero convertido
+ * @return double 
+ */
 double conversionMoneda(double dinero, int tipo_moneda){
     if (tipo_moneda == 1){
         dinero = dinero/514.26;
@@ -8,12 +40,4 @@ double conversionMoneda(double dinero, int tipo_moneda){
         dinero = dinero*514.26;
         return dinero;
     }
-}
-
-int main(){
-    double dinero = 1000;
-    int tipo_moneda = 2;
-    dinero = conversionMoneda(dinero, tipo_moneda);
-    std::cout << dinero;
-    return 0;
 }
