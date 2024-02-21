b2ad9eee894d64ee6f63be972285217576310f3d
BraiderZ <luisjbc2112@gmail.com>
2024-02-21 11:05:14 -0600

    Se documentaron los archivos de los que se encarga este branch
    Signed-off-by: BraiderZ <luisjbc2112@gmail.com>

--- cuentasAhorros.cpp
+++ cuentasAhorros.cpp
@@ -1,11 +1,40 @@
 #include "cuentasAhorros.hpp"
 
+/**
+ * @file cuentasAhorros.cpp
+ * @brief Funcionalidades del los diferentes métodos de la clase CuentasAhorros.
+ * 
+ * @license 
+ * Copyright 2024 [Brenda Natalia Castro Jiménez, Emanuel Avilés Ramírez, Luis José Brenes Campos]
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+*/
+
+// Constructor que almacena la cédula del usuario realiza el trámite
 CuentasAhorros::CuentasAhorros(long long int cedula) : cedula_cliente(cedula) {}
 
+/**
+ * @brief Crea una cuenta de ahorros para el usuario
+ * 
+ * @return true 
+ * @return false 
+ */
 bool CuentasAhorros::crearCuenta(){
     try{
         int tipo;
         
+        // Verifica que el usuario no posea la cantidad de cuentas máxima
         if (cantidad_cuentas == 2){
             std::cout << "Usted ya posee 2 cuentas" << std::endl;
             return false;
@@ -26,6 +55,7 @@ bool CuentasAhorros::crearCuenta(){
             throw std::runtime_error("No ha colocado un numero para el tipo de moneda");
         }
 
+        // Almacena el tipo de moneda de la cuenta que se va a crear
         switch (tipo){
         case 1:
             tipo_moneda = 1;
@@ -48,7 +78,9 @@ bool CuentasAhorros::crearCuenta(){
         
         bool verificando;
 
+        // Verificamos que no exista una cuenta con las características elegidas
         verificando = verificarCuentaAhorrosRepetida();
+        // En caso de que se cumpla, se guarda la nueva cuenta
         if (verificando == true){
             guardarDatos();
             return true;
@@ -62,8 +94,18 @@ bool CuentasAhorros::crearCuenta(){
     }
 }
 
+/**
+ * @brief Verifica que el usuario no posea cuentas repetidas
+ * 
+ * @return true 
+ * @return false 
+ */
 bool CuentasAhorros::verificarCuentaAhorrosRepetida(){
-    
+    /**
+     * La lógica que se observa aquí, es utilizada a lo largo del código para obtener los datos que se encuentran en el txt
+     * Estos datos se revisan línea por línea verificando condiciones
+     * 
+     */
     std::ifstream archivo_entrada("cuentasAhorros.txt");
 
     if (archivo_entrada.is_open()) {
@@ -78,6 +120,7 @@ bool CuentasAhorros::verificarCuentaAhorrosRepetida(){
             if (ss >> id >> std::ws && ss.ignore() && ss >> tipo_moneda_archivo >> std::ws && ss.ignore() && ss >> dinero) {
 
                 if (id == cedula_cliente) {
+                    // Si el usuario ya posee una cuenta del mismo tipo al elegido, no se le perimite tener otra igual
                     if (tipo_moneda_archivo == tipo_moneda){
                         std::cout << "Este usuario ya posee una cuenta de este tipo" <<  std::endl;
                         std::cout << "Intente otra vez..." << std::endl;
@@ -101,9 +144,14 @@ bool CuentasAhorros::verificarCuentaAhorrosRepetida(){
     }
 }
 
+/**
+ * @brief Guarda los daotos iniciales de una cuenta dentro del txt correspondiente
+ * 
+ */
 void CuentasAhorros::guardarDatos(){
     
     std::ofstream archivo("cuentasAhorros.txt", std::ios::app);
+    // Se establece una precisión de 15 dígitos en el dinero de la cuenta para evitar pérdida de información 
     if (archivo.is_open()) {
         archivo << this->cedula_cliente << "," << tipo_moneda << ","
         << std::fixed << std::setprecision(15) << this->dinero_cuenta << std::endl;
@@ -113,6 +161,10 @@ void CuentasAhorros::guardarDatos(){
     }
 }
 
+/**
+ * @brief Por medio de switch-case, el usuario puede elegir las múltiples acciones sobre sus cuentas de ahorros
+ * 
+ */
 void CuentasAhorros::menuAccionesCuenta(){
     int opcion;
     bool verificar;
@@ -202,6 +254,10 @@ void CuentasAhorros::menuAccionesCuenta(){
     }
 }
 
+/**
+ * @brief Imprime las cuentas de ahorros que posee el usuario
+ * 
+ */
 void CuentasAhorros::verMisCuentas(){
     std::ifstream archivo_entrada("cuentasAhorros.txt");
 
@@ -219,7 +275,7 @@ void CuentasAhorros::verMisCuentas(){
             std::istringstream ss(linea);
 
             if (ss >> id >> std::ws && ss.ignore() && ss >> tipo_moneda_archivo >> std::ws && ss.ignore() && ss >> dinero) {
-
+                // Se imprime el tipo de moneda de cada cuenta
                 if (id == cedula_cliente) {
                     std::cout << std::fixed << std::setprecision(15) << dinero << std::endl;
                     if (tipo_moneda_archivo == 1){
@@ -246,6 +302,10 @@ void CuentasAhorros::verMisCuentas(){
     }
 }
 
+/**
+ * @brief Verifica la cantidad de cuentas que posee el cliente
+ * 
+ */
 void CuentasAhorros::verficarCantidadCuentas(){
     std::ifstream archivo_entrada("cuentasAhorros.txt");
 
@@ -260,6 +320,7 @@ void CuentasAhorros::verficarCantidadCuentas(){
 
             if (ss >> id >> std::ws && ss.ignore() && ss >> tipo_moneda_archivo >> std::ws && ss.ignore() && ss >> dinero) {
 
+                // Suma una cuenta al atributo del usuario de la cantidad de cuentas
                 if (id == cedula_cliente  && cedula_cliente_otro == 0) {
                     cantidad_cuentas = cantidad_cuentas + 1;
                 }else if (id == cedula_cliente_otro){
@@ -281,6 +342,10 @@ void CuentasAhorros::verficarCantidadCuentas(){
     }
 }
 
+/**
+ * @brief Elije la cuenta que el usuario desea utilizar para relizar los trámites
+ * 
+ */
 void CuentasAhorros::elegirCuenta(){
     std::ifstream archivo_entrada("cuentasAhorros.txt");
 
@@ -290,6 +355,13 @@ void CuentasAhorros::elegirCuenta(){
         double dinero;
         std::string linea;
 
+        /**
+         * Se establecen primero condiciones dobles:
+         * Si el usuario posee una sola cuenta y no se ha modifica la cantidad de cuentas del otro usuario. Se guardan los datos de la cuenta del cliente en los atributos
+         * Esta condición es parecida en caso de poseer dos cuentas pero, ahora tiene que elgir una de las dos cuentas para obtener los datos
+         * Si la cantidad de cuentas es 0, funciona como flag para que obtenga los datos de la otra cuenta en el caso de que se este realizando una trasferencia entre cuentas propias
+         * La condición de que la cantidad de cuentas del otro usuario sea 0, funciona como flag para reconocer si ya se realizó la elección de la cuenta que utiliza el cliente y ahora queremos obtemer los datos del otro usuario
+         */
         if (cantidad_cuentas == 1 && cantidad_cuentas_otro == 0){
             while (std::getline(archivo_entrada, linea)) {
                 std::istringstream ss(linea);
@@ -374,6 +446,7 @@ void CuentasAhorros::elegirCuenta(){
                     }
                 }
             }
+        // Condiciones parecidas pero para obtener los datos del otro usuario cuando posee una o dos cuentas
         }else if (cantidad_cuentas_otro == 1){
             while (std::getline(archivo_entrada, linea)) {
                 std::istringstream ss(linea);
@@ -456,6 +529,13 @@ void CuentasAhorros::elegirCuenta(){
     }
 }
 
+/**
+ * @brief El usuario ingresa dinero a la cuenta de ahorros
+ * 
+ * El booleano se encarga de determinar si se deben actualizar los datos
+ * @return true 
+ * @return false 
+ */
 bool CuentasAhorros::deposito(){
     double dinero_depositado;
     
@@ -474,19 +554,27 @@ bool CuentasAhorros::deposito(){
     return true;
 }
 
+/**
+ * @brief El usuario retira dinero de la cuenta de ahorros
+ * 
+ * El booleano se encarga de determinar si se deben actualizar los datos
+ * @return true 
+ * @return false 
+ */
 bool CuentasAhorros::retiro(double retirar){
     if (retirar == 0){    
         double dinero_retirado;
         
         std::cout << "Ingrese la cantidad de dinero a retirar a su cuenta: ";
         std::cin >> dinero_retirado;
-
+        
         if (std::cin.fail() || dinero_retirado < 0){
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             std::cout << "Ha elegido una cantidad de dinero erronea a retirar" << std::endl;
             std::cout << std::endl;
             return false;
+        // Verifica si posee el dinero suficiente en la cuenta para realizar el retiro
         }else if (dinero_retirado > dinero_cuenta){
             std::cout << "Su cuenta posee menos dinero del que pide retirar" << std::endl;
             std::cout << std::endl;
@@ -502,7 +590,12 @@ bool CuentasAhorros::retiro(double retirar){
     
 }
 
+/**
+ * @brief Actualiza los datos en caso de realizar algún trámite
+ * 
+ */
 void CuentasAhorros::actualizarDatos(){
+    // Obtiene los datos de el archivo y crea uno nuevo
     std::ifstream archivo_lectura("cuentasAhorros.txt");
     std::ofstream archivo_escritura("cuentasAhorros_temp.txt");
 
@@ -511,6 +604,7 @@ void CuentasAhorros::actualizarDatos(){
         long long int id;
         int tipo_moneda_archivo;
         double dinero;
+        // Verifica si una línea del archivo original cambió
         bool cambio = false;
         std::string linea;
 
@@ -519,7 +613,7 @@ void CuentasAhorros::actualizarDatos(){
             std::istringstream ss(linea);
 
             if (ss >> id >> std::ws && ss.ignore() && ss >> tipo_moneda_archivo >> std::ws && ss.ignore() && ss >> dinero) {
-
+                // Cada línea la copia dentro del nuevo archivo txt
                 if (id == cedula_cliente && tipo_moneda_archivo == tipo_moneda) {
                     dinero = dinero_cuenta;
                     cambio = true;
@@ -543,6 +637,7 @@ void CuentasAhorros::actualizarDatos(){
         archivo_lectura.close();
         archivo_escritura.close();
 
+        // En caso de que cambien datos del archivo original, se elimina y el archivo temporal para a poseer su nombre
         if (cambio) {
             remove("cuentasAhorros.txt");
             rename("cuentasAhorros_temp.txt", "cuentasAhorros.txt");
@@ -556,12 +651,22 @@ void CuentasAhorros::actualizarDatos(){
     }
 }
 
+/**
+ * @brief Tranfiere dinero de una cuentra propia a otra
+ * 
+ * El booleano se encarga de determinar si se deben actualizar los datos
+ * @return true 
+ * @return false 
+ */
 bool CuentasAhorros::tranferirDineroPropia(){
     double dinero_transferir = 0;
     
+    // Primero se debe verificar que el usuario posea 2 cuentas
     if (cantidad_cuentas == 1){
         std::cout << "Usted solo posee una cuenta, no puede realizar este proceso" << std::endl;
+        return false;
     } else{
+        // Dependiendo de si se elije la cuenta en colones o en dólares para realizar el trámite, se imprimen los mensajes
         if (tipo_moneda == 1){
            std::cout << "Digite la cantidad de dinero de su cuenta en colones que desea tranferir a su cuenta en dólares:  ";
            std::cin >> dinero_transferir;
@@ -580,15 +685,22 @@ bool CuentasAhorros::tranferirDineroPropia(){
 
             }
 
+            // Se cambia la cantidad de dinero del atributo y se actualizan los datos
             dinero_cuenta = dinero_cuenta - dinero_transferir;
+            // Como se ssabe que las dos cuentas son de diferente  tipo de moneda, se convierte el dinero al otro tipo de maneda antes de transferirla
             dinero_transferir = conversionMoneda(dinero_transferir, tipo_moneda);
             actualizarDatos();
+            // Se coloca el flag 0 para obtener la otra cuenta Esto se logra con la condición mencionada anteriormente y a que se cambia el tipo de moneda al contrario de la cuenta elegida para el trámite
             cantidad_cuentas = 0;
             tipo_moneda = 2;
+            // Se obtienen los datos de la otra cuenta
             elegirCuenta();
+            // Se el deposita el dinero transferido
             dinero_cuenta = dinero_cuenta + dinero_transferir;
+            // Se actualiza dicha cuenta
             actualizarDatos();
             return true;
+        // Mismo proceso pero para la cuenta en dólares
         }else{
             std::cout << "Digite la cantidad de dinero de su cuenta en dólares que desea tranferir a su cuenta en colones:  ";
             std::cin >> dinero_transferir;
@@ -621,6 +733,12 @@ bool CuentasAhorros::tranferirDineroPropia(){
     }
 }
 
+/**
+ * @brief Se elije el otro usuario al que se le transfiere el dinero
+ * 
+ * Retorna la cédula del otro usuario para posteriormente obtener los datos de su cuenta
+ * @return long long int 
+ */
 long long int CuentasAhorros::elegirOtroUsuario(){
     std::string nombre;
     std::string cedula_str;
@@ -681,6 +799,12 @@ long long int CuentasAhorros::elegirOtroUsuario(){
     return 0;
 }
 
+/**
+ * @brief Funcionalidad parecida a la de retirar dinero pero ahora para transferir dinero a otro usuario
+ * 
+ * @return true 
+ * @return false 
+ */
 bool CuentasAhorros::tranferirDineroOtro(){
     double dinero_transferir = 0;
     
@@ -703,6 +827,7 @@ bool CuentasAhorros::tranferirDineroOtro(){
 
         dinero_cuenta = dinero_cuenta - dinero_transferir;
         actualizarDatos();
+        // En caso de que la cuenta del otro usuario no sea del mismo tipo de moneda que la del cliente, se convierte el dinero antes de depositarlo
         if (tipo_moneda != tipo_moneda_otro){
             dinero_transferir = conversionMoneda(dinero_transferir, tipo_moneda);
             dinero_cuenta_otro = dinero_cuenta_otro + dinero_transferir;
